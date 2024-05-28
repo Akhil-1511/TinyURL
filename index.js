7cf76a10748464543924e6bf44426d472eea27d1
gourishetty akhil <akhilsai1511@gmail.com>
2024-05-28 10:55:53 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const express=require('express');
+const path=require("path");
+const cookieParser=require("cookie-parser");
+
+const urlRoute=require("./routes/url");
+const staticRouter=require("./routes/staticRouter");
+const userRoute=require("./routes/user");
+
+const {connectToMongoDB} =require("./connect");
+const {checkFoRAuthentication,restrictTo}=require("./middlewares/auth");
+const URL=require('./models/url');
+
+const app=express();
+const PORT=8001;
+
+
+
+connectToMongoDB("mongodb://127.0.0.1:27017/short-url")
+.then(()=>{
+    console.log("Database connected");
+});
+app.set("view engine","ejs");
+app.set("views",path.resolve("./views"));
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(cookieParser());
+
+app.use(checkFoRAuthentication);
+
+
+
+
+
+
+app.use("/url",restrictTo(["NORMAL","ADMIN"]),urlRoute);
+app.use("/user",userRoute);
+app.use("/",staticRouter);
+
+
+
+app.get('/url/:shortId',async(req,res)=>{
+    const shortId= req.params.shortId;
+    const entry=await URL.findOneAndUpdate({
+        shortId
+    },{$push:{
+        visitHistory:{
+            timestamp:Date.now(),
+        },
+    },
+}
+);
+res.redirect(entry.redirectURL);
+});
+app.listen(PORT,()=> console.log(`server has started at ${PORT}`))
